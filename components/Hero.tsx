88681ccdbcb534431b96be57934300b14898f836
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2023-12-07 21:03:58 +0530

    Original COde

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,200 +1,170 @@
-import { useAnimate } from "framer-motion";
-import React, { useRef } from "react";
+import Image from 'next/image';
+import React, { useEffect } from 'react';
+import a from "../assets/image-track/a.png";
+import b from "../assets/image-track/b.png";
+import c from "../assets/image-track/c.png";
+import d from "../assets/image-track/d.png";
+import e from "../assets/image-track/e.png";
+import f from "../assets/image-track/f.png";
+import g from "../assets/image-track/g.png";
+import h from "../assets/image-track/h.jpg";
+
 const Hero = () => {
-  return (
-    <MouseImageTrail
-      renderImageBuffer={50}
-      rotationRange={25}
-      images={[
-        "https://images.unsplash.com/photo-1663583513676-9f6361cd859d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-        "https://images.unsplash.com/photo-1663530294185-5af3692326c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1506&q=80",
-        "https://images.unsplash.com/photo-1663579111009-863bc978c78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-        "https://images.unsplash.com/photo-1663492412083-17bfcad3c533?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-        "https://images.unsplash.com/photo-1663571473113-d3fc49bbe775?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80",
-        "https://images.unsplash.com/photo-1663431263243-ef4aa402afc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-        "https://images.unsplash.com/photo-1663330082092-11fa01e1ee8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-        "https://images.unsplash.com/photo-1663450806250-da193dc36368?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1149&q=80",
-        "https://images.unsplash.com/photo-1663352248740-645afa021c9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-        "https://images.unsplash.com/photo-1663431905837-09cf339461ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2207&q=80",
-      ]}
-    >
-      <section className="mx-auto max-w-screen-2xl md:px-8 px-8 text-white mb-10">
-     
-  <div className="mx-auto max-w-screen-xl px-4 lg:flex  lg:items-center py-72">
-    <div className="mx-auto max-w-3xl text-center">
-      <h1
-        className="bg-gradient-to-r from-blue-600 via-green-400 to-green-300 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
-      >
-       CARBON LEDGER
+  const [mouseDownAt, setMouseDownAt] = React.useState<number | null>(null);
+  const [prevPercentage, setPrevPercentage] = React.useState<number>(0);
+  const trackRef = React.useRef<HTMLDivElement | null>(null);
+  let nextPercentage = 0;
+  let nextPercentageUnconstrained = 0;
+  const autoAnimationDuration = 1000;
 
-      </h1>
-        <span className="sm:block text-2xl sm:text-4xl font-extrabold"> Find You're Green </span>
+  if (trackRef.current) {
+    (trackRef.current as HTMLDivElement).dataset.prevPercentage = "0";
+  }
 
-      <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-      A concise record of emissions and offsets, driving transparency for a sustainable, low-carbon future.
-      </p>
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setMouseDownAt(e.clientX);
+  };
 
-      {/* <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <a
-          className="block w-full rounded border border-green-300 bg-green-400 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-          href="/get-started"
-        >
-          Get Started
-        </a>
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (mouseDownAt === null) return;
 
-        <a
-          className="block w-full rounded border border-green-300 px-12 py-3 text-sm font-medium text-white hover:bg-green-400 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-          href="/about"
-        >
-          Learn More
-        </a>
-      </div> */}
-    </div>
-  </div>
+    const mouseDelta = parseFloat(mouseDownAt.toString()) - e.clientX;
+    const maxDelta = window.innerWidth / 2;
 
+    const percentage = (mouseDelta / maxDelta) * -100;
 
-    <div className="flex flex-col items-center justify-between gap-8 md:flex-row px-16">
-      <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
-        <a href="#" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Carbon</a>
-        <a href="#" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Ledger</a>
-        <a href="#" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Company</a>
-      </div>
+    nextPercentageUnconstrained = prevPercentage + percentage;
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
-      <div className="flex items-center justify-center gap-4 lg:justify-start">
-        <span className="text-sm font-semibold uppercase tracking-widest text-gray-400 sm:text-base">Social</span>
-        <span className="h-px w-12 bg-gray-200"></span>
+    if (trackRef.current) {
+      const datasetMouseDownAt = (trackRef.current as HTMLDivElement).dataset.mouseDownAt;
+      if (datasetMouseDownAt === undefined) return;
 
-        <div className="flex gap-4">
-          <a href="#" target="_blank" className="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
-            <svg className="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-            </svg>
-          </a>
+      const parsedMouseDownAt = parseFloat(datasetMouseDownAt);
+      if (!isNaN(parsedMouseDownAt)) {
+        (trackRef.current as HTMLDivElement).animate(
+          {
+            transform: `translate(${nextPercentage}%, -50%)`,
+          },
+          {
+            duration: 1500,
+            fill: 'forwards',
+          }
+        );
 
-          <a href="#" target="_blank" className="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
-            <svg className="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-            </svg>
-          </a>
+        const trackImages = (trackRef.current as HTMLDivElement).querySelectorAll('.trackImage');
+        trackImages.forEach((image) => {
+          (image as HTMLElement).animate(
+            {
+              objectPosition: `${100 + nextPercentage}% center`,
+            },
+            {
+              duration: 1200,
+              fill: 'forwards',
+            }
+          );
+        });
 
-          <a href="#" target="_blank" className="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
-            <svg className="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path d="M19.4168 2.4594C17.7648 0.873472 15.4785 0 12.9793 0C9.1616 0 6.81353 1.56493 5.51603 2.87767C3.91693 4.49547 3 6.64362 3 8.77138C3 11.4429 4.11746 13.4934 5.98876 14.2563C6.11439 14.3078 6.24081 14.3337 6.36475 14.3337C6.75953 14.3337 7.07233 14.0754 7.1807 13.661C7.24389 13.4233 7.39024 12.8369 7.45389 12.5823C7.59011 12.0795 7.48005 11.8377 7.18295 11.4876C6.64173 10.8472 6.38969 10.0899 6.38969 9.10438C6.38969 6.17698 8.56948 3.06578 12.6095 3.06578C15.8151 3.06578 17.8064 4.88772 17.8064 7.82052C17.8064 9.67124 17.4077 11.3852 16.6837 12.6468C16.1805 13.5235 15.2957 14.5685 13.9375 14.5685C13.3501 14.5685 12.8225 14.3272 12.4896 13.9066C12.1751 13.5089 12.0714 12.9953 12.1979 12.4599C12.3408 11.855 12.5357 11.2241 12.7243 10.6141C13.0682 9.5001 13.3933 8.44789 13.3933 7.60841C13.3933 6.17252 12.5106 5.20769 11.1969 5.20769C9.52737 5.20769 8.21941 6.90336 8.21941 9.06805C8.21941 10.1297 8.50155 10.9237 8.62929 11.2286C8.41896 12.1197 7.16899 17.4176 6.93189 18.4166C6.79478 18.9997 5.96893 23.6059 7.33586 23.9731C8.87168 24.3858 10.2445 19.8997 10.3842 19.3928C10.4975 18.9806 10.8937 17.4216 11.1365 16.4634C11.878 17.1775 13.0717 17.6603 14.2333 17.6603C16.4231 17.6603 18.3924 16.6749 19.7786 14.8858C21.1229 13.1505 21.8633 10.7318 21.8633 8.0757C21.8632 5.99923 20.9714 3.95209 19.4168 2.4594Z" />
-            </svg>
-          </a>
-        </div>
-      </div>
-   
-    </div>
-  </section>
-    </MouseImageTrail>
-  );
-};
-
-const MouseImageTrail = ({
-  children,
-  // List of image sources
-  images,
-  // Will render a new image every X pixels between mouse moves
-  renderImageBuffer,
-  // images will be rotated at a random number between zero and rotationRange,
-  // alternating between a positive and negative rotation
-  rotationRange,
-}) => {
-  const [scope, animate] = useAnimate();
-
-  const lastRenderPosition = useRef({ x: 0, y: 0 });
-  const imageRenderCount = useRef(0);
-
-  const handleMouseMove = (e) => {
-    const { clientX, clientY } = e;
-
-    const distance = calculateDistance(
-      clientX,
-      clientY,
-      lastRenderPosition.current.x,
-      lastRenderPosition.current.y
-    );
-
-    if (distance >= renderImageBuffer) {
-      lastRenderPosition.current.x = clientX;
-      lastRenderPosition.current.y = clientY;
-
-      renderNextImage();
+        (trackRef.current as HTMLDivElement).dataset.prevPercentage = nextPercentage.toString();
+      }
     }
   };
 
-  const calculateDistance = (x1, y1, x2, y2) => {
-    const deltaX = x2 - x1;
-    const deltaY = y2 - y1;
-
-    // Using the Pythagorean theorem to calculate the distance
-    const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-
-    return distance;
+  const onMouseUp = () => {
+    // Reset the data-mouse-down-at attribute to "0" when the mouse button is released
+    if (trackRef.current) {
+      (trackRef.current as HTMLDivElement).dataset.mouseDownAt = "0";
+      (trackRef.current as HTMLDivElement).dataset.prevPercentage = (trackRef.current as HTMLDivElement).dataset.percentage;
+      setPrevPercentage(nextPercentage);
+      console.log("prev ", (trackRef.current as HTMLDivElement).dataset);
+    }
   };
 
-  const renderNextImage = () => {
-    const imageIndex = imageRenderCount.current % images.length;
-    const selector = `[data-mouse-move-index="${imageIndex}"]`;
+  const startAutoAnimation = () => {
+    let autoAnimationDirection = 1; // 1 for increasing, -1 for decreasing
+    setInterval(() => {
+      const currentPercentageString = (trackRef.current as HTMLDivElement).dataset.prevPercentage;
 
-    const el = document.querySelector(selector);
+      if (currentPercentageString !== undefined) {
+        const currentPercentage = parseFloat(currentPercentageString);
+        if (!isNaN(currentPercentage)) {
+          let nextPercentage = currentPercentage + autoAnimationDirection;
 
-    el.style.top = `${lastRenderPosition.current.y}px`;
-    el.style.left = `${lastRenderPosition.current.x}px`;
-    el.style.zIndex = imageRenderCount.current.toString();
+          if (nextPercentage >= 0) {
+            nextPercentage = 0;
+            autoAnimationDirection = -autoAnimationDirection;
+          } else if (nextPercentage <= -100) {
+            nextPercentage = -100;
+            autoAnimationDirection = -autoAnimationDirection;
+          }
 
-    const rotation = Math.random() * rotationRange;
+          if (trackRef.current) {
+            (trackRef.current as HTMLDivElement).animate(
+              {
+                transform: `translate(${nextPercentage}%, -50%)`,
+              },
+              {
+                duration: autoAnimationDuration,
+                fill: 'forwards',
+              }
+            );
 
-    animate(
-      selector,
-      {
-        opacity: [0, 1],
-        transform: [
-          `translate(-50%, -25%) scale(0.5) ${
-            imageIndex % 2
-              ? `rotate(${rotation}deg)`
-              : `rotate(-${rotation}deg)`
-          }`,
-          `translate(-50%, -50%) scale(1) ${
-            imageIndex % 2
-              ? `rotate(-${rotation}deg)`
-              : `rotate(${rotation}deg)`
-          }`,
-        ],
-      },
-      { type: "spring", damping: 15, stiffness: 200 }
-    );
+            const trackImages = (trackRef.current as HTMLDivElement).querySelectorAll('.trackImage');
+            trackImages.forEach((image) => {
+              (image as HTMLElement).animate(
+                {
+                  objectPosition: `${100 + nextPercentage}% center`,
+                },
+                {
+                  duration: 1200,
+                  fill: 'forwards',
+                }
+              );
+            });
 
-    animate(
-      selector,
-      {
-        opacity: [1, 0],
-      },
-      { ease: "linear", duration: 0.5, delay: 5 }
-    );
-
-    imageRenderCount.current = imageRenderCount.current + 1;
+            (trackRef.current as HTMLDivElement).dataset.prevPercentage = nextPercentage.toString();
+          }
+        }
+      }
+    }, autoAnimationDuration);
   };
+
+  useEffect(() => {
+    // Start the automatic animation loop when the component mounts
+    startAutoAnimation();
+  }, []);
 
   return (
-    <div
-      ref={scope}
-      className="relative overflow-visible"
-      onMouseMove={handleMouseMove}
-    >
-      {children}
-
-      {images.map((img, index) => (
-        <img
-          className="pointer-events-none absolute left-0 top-0 h-48 w-auto rounded-xl border-2 border-black bg-neutral-900 object-cover opacity-0"
-          src={img}
-          alt={`Mouse move image ${index}`}
-          key={index}
-          data-mouse-move-index={index}
-        />
-      ))}
-    </div>
+    <>
+      <div className="bg-none w-full h-screen overflow-hidden m-0 flex flex-col  items-center text-white relative">
+        <div className="mt-24 text-center">
+          <h1 className="text-5xl uppercase magic">
+            <span className="magic-text font-bold">E-State Ledger</span>
+          </h1>
+          <p className="text-2xl capitalize">Bridging the Gap between Real State and Blockchain</p>
+        </div>
+        <div
+          id="image-track"
+          className="overflow-hidden"
+          data-mouse-down-at={mouseDownAt}
+          data-prev-percentage={prevPercentage}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={onMouseUp}
+          ref={trackRef}
+        >
+                      <Image src={a} alt="Image A" className="trackImage" draggable="false" />
+        <Image src={b} alt="Image A" className="trackImage" draggable="false" />
+        <Image src={c} alt="Image A" className="trackImage" draggable="false" />
+        <Image src={d} alt="Image A" className="trackImage" draggable="false" />
+        <Image src={e} alt="Image A" className="trackImage" draggable="false" />
+        <Image src={f} alt="Image A" className="trackImage" draggable="false" />
+        <Image src={g} alt="Image A" className="trackImage" draggable="false" />
+        <Image src={h} alt="Image A" className="trackImage" draggable="false" />
+        </div>
+      </div>
+    </>
   );
 };
+
 export default Hero;
