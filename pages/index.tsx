120319af9c2b2d8723017dd8c2aaf08a8ae5c424
Rahat Sayyed <70068895+youreachedrahat@users.noreply.github.com>
2023-12-08 10:05:20 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,12 @@ export default function Home() {
       <div className="z-10 relative">
         
         <Hero />
+         <div className="w-[80%] mb-5">
+          <h2 className="text-white text-center font-semibold text-5xl leading-loose uppercase tracking-wide">
+          Carbon Offset Project Across world
+        </h2>
         <Maps />
+        </div>
         <HomeListings />
         <CommunityBoard />
         {connected ? <MarketPlace /> : <></>}
