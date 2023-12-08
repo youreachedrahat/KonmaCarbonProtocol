32bf470865779aac1b7ed48bebc899c285ed00ee
Rahat Sayyed <70068895+youreachedrahat@users.noreply.github.com>
2023-12-08 10:35:44 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="z-10 relative">
         
         <Hero />
-         <div className="w-[80%] mb-5 mx-auto">
+         <div className="w-[80%] mb-5 mx-auto rounded-sm overflow-hidden">
           <h2 className="text-white text-center font-semibold text-5xl leading-loose uppercase tracking-wide">
           Carbon Offset Project Across world
         </h2>
